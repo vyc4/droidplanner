2649d27b80f8bb57d94d98c52cfb7b732a869f6e
Victor Chen <vyc4@duke.edu>
2015-04-13 09:29:55 -0400

    Removed all telemetry

--- Android/src/org/droidplanner/android/fragments/TelemetryFragment.java
+++ Android/src/org/droidplanner/android/fragments/TelemetryFragment.java
@@ -53,49 +53,49 @@ public class TelemetryFragment extends ApiListenerFragment {
             final String action = intent.getAction();
             final Drone drone = getDrone();
 
-            switch (action) {
-                case AttributeEvent.ATTITUDE_UPDATED:
-                    final Attitude attitude = drone.getAttribute(AttributeType.ATTITUDE);
-                    onOrientationUpdate(attitude);
-                    break;
-
-                case AttributeEvent.SPEED_UPDATED:
-                    final Speed droneSpeed = drone.getAttribute(AttributeType.SPEED);
-                    onSpeedUpdate(droneSpeed);
-
-                    final Altitude droneAltitude = drone.getAttribute(AttributeType.ALTITUDE);
-                    onAltitudeUpdate(droneAltitude);
-                    break;
-
-                case AttributeEvent.STATE_UPDATED:
-                    updateFlightTimer();
-                    break;
-            }
+//            switch (action) {
+//                case AttributeEvent.ATTITUDE_UPDATED:
+//                    final Attitude attitude = drone.getAttribute(AttributeType.ATTITUDE);
+//                    onOrientationUpdate(attitude);
+//                    break;
+//
+//                case AttributeEvent.SPEED_UPDATED:
+//                    final Speed droneSpeed = drone.getAttribute(AttributeType.SPEED);
+//                    onSpeedUpdate(droneSpeed);
+//
+//                    final Altitude droneAltitude = drone.getAttribute(AttributeType.ALTITUDE);
+//                    onAltitudeUpdate(droneAltitude);
+//                    break;
+//
+//                case AttributeEvent.STATE_UPDATED:
+//                    updateFlightTimer();
+//                    break;
+//            }
         }
     };
 
     /**
      * Runnable used to update the drone flight time.
      */
-    protected Runnable mFlightTimeUpdater = new Runnable() {
-        @Override
-        public void run() {
-            mHandler.removeCallbacks(this);
-            final Drone drone = getDrone();
-            if (drone == null || !drone.isConnected())
-                return;
-
-            if (flightTimer != null) {
-                long timeInSeconds = drone.getFlightTime();
-                long minutes = timeInSeconds / 60;
-                long seconds = timeInSeconds % 60;
-
-                flightTimer.setText(String.format("%02d:%02d", minutes, seconds));
-            }
-
-            mHandler.postDelayed(this, FLIGHT_TIMER_PERIOD);
-        }
-    };
+//    protected Runnable mFlightTimeUpdater = new Runnable() {
+//        @Override
+//        public void run() {
+//            mHandler.removeCallbacks(this);
+//            final Drone drone = getDrone();
+//            if (drone == null || !drone.isConnected())
+//                return;
+//
+//            if (flightTimer != null) {
+//                long timeInSeconds = drone.getFlightTime();
+//                long minutes = timeInSeconds / 60;
+//                long seconds = timeInSeconds % 60;
+//
+//                flightTimer.setText(String.format("%02d:%02d", minutes, seconds));
+//            }
+//
+//            mHandler.postDelayed(this, FLIGHT_TIMER_PERIOD);
+//        }
+//    };
 
     /**
      * This handler is used to update the flight time value.
@@ -117,48 +117,48 @@ public class TelemetryFragment extends ApiListenerFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_telemetry, container, false);
-        attitudeIndicator = (AttitudeIndicator) view.findViewById(R.id.aiView);
-
-        roll = (TextView) view.findViewById(R.id.rollValueText);
-        yaw = (TextView) view.findViewById(R.id.yawValueText);
-        pitch = (TextView) view.findViewById(R.id.pitchValueText);
-
-        groundSpeed = (TextView) view.findViewById(R.id.groundSpeedValue);
-        airSpeed = (TextView) view.findViewById(R.id.airSpeedValue);
-        climbRate = (TextView) view.findViewById(R.id.climbRateValue);
-        altitude = (TextView) view.findViewById(R.id.altitudeValue);
-
-        flightTimer = (TextView) view.findViewById(R.id.flight_timer);
+//        attitudeIndicator = (AttitudeIndicator) view.findViewById(R.id.aiView);
+//
+//        roll = (TextView) view.findViewById(R.id.rollValueText);
+//        yaw = (TextView) view.findViewById(R.id.yawValueText);
+//        pitch = (TextView) view.findViewById(R.id.pitchValueText);
+//
+//        groundSpeed = (TextView) view.findViewById(R.id.groundSpeedValue);
+//        airSpeed = (TextView) view.findViewById(R.id.airSpeedValue);
+//        climbRate = (TextView) view.findViewById(R.id.climbRateValue);
+//        altitude = (TextView) view.findViewById(R.id.altitudeValue);
+//
+//        flightTimer = (TextView) view.findViewById(R.id.flight_timer);
 
         final Resources res = getResources();
         final int popupWidth = ViewGroup.LayoutParams.WRAP_CONTENT;
         final int popupHeight = ViewGroup.LayoutParams.WRAP_CONTENT;
         final Drawable popupBg = res.getDrawable(android.R.color.transparent);
 
-        final View resetTimerView = inflater.inflate(R.layout.popup_info_flight_time, container, false);
-        final PopupWindow resetTimerPopup = new PopupWindow(resetTimerView, popupWidth, popupHeight, true);
-        resetTimerPopup.setBackgroundDrawable(popupBg);
-
-        final TextView resetTimer = (TextView) resetTimerView.findViewById(R.id.bar_flight_time_reset_timer);
-        resetTimer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Drone drone = getDrone();
-                if(drone != null && drone.isConnected())
-                    drone.resetFlightTimer();
-
-                updateFlightTimer();
-                resetTimerPopup.dismiss();
-            }
-        });
-
-        final ImageButton resetFlightTimerButton = (ImageButton) view.findViewById(R.id.reset_flight_timer_button);
-        resetFlightTimerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetTimerPopup.showAsDropDown(resetFlightTimerButton);
-            }
-        });
+//        final View resetTimerView = inflater.inflate(R.layout.popup_info_flight_time, container, false);
+//        final PopupWindow resetTimerPopup = new PopupWindow(resetTimerView, popupWidth, popupHeight, true);
+//        resetTimerPopup.setBackgroundDrawable(popupBg);
+//
+//        final TextView resetTimer = (TextView) resetTimerView.findViewById(R.id.bar_flight_time_reset_timer);
+//        resetTimer.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                final Drone drone = getDrone();
+//                if(drone != null && drone.isConnected())
+//                    drone.resetFlightTimer();
+//
+//                updateFlightTimer();
+//                resetTimerPopup.dismiss();
+//            }
+//        });
+//
+//        final ImageButton resetFlightTimerButton = (ImageButton) view.findViewById(R.id.reset_flight_timer_button);
+//        resetFlightTimerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                resetTimerPopup.showAsDropDown(resetFlightTimerButton);
+//            }
+//        });
 
         return view;
     }
@@ -171,19 +171,19 @@ public class TelemetryFragment extends ApiListenerFragment {
         headingModeFPV = prefs.getBoolean("pref_heading_mode", false);
     }
 
-    private void updateFlightTimer(){
-        final Drone drone = getDrone();
-        mHandler.removeCallbacks(mFlightTimeUpdater);
-        if (drone != null && drone.isConnected()) {
-            mFlightTimeUpdater.run();
-        } else {
-            flightTimer.setText("00:00");
-        }
-    }
+//    private void updateFlightTimer(){
+//        final Drone drone = getDrone();
+//        mHandler.removeCallbacks(mFlightTimeUpdater);
+//        if (drone != null && drone.isConnected()) {
+//            mFlightTimeUpdater.run();
+//        } else {
+//            flightTimer.setText("00:00");
+//        }
+//    }
 
     @Override
     public void onApiConnected() {
-        updateFlightTimer();
+//        updateFlightTimer();
         getBroadcastManager().registerReceiver(eventReceiver, eventFilter);
     }
 
@@ -192,42 +192,42 @@ public class TelemetryFragment extends ApiListenerFragment {
         getBroadcastManager().unregisterReceiver(eventReceiver);
     }
 
-    public void onOrientationUpdate(Attitude droneAttitude) {
-        if (droneAttitude == null)
-            return;
-
-        float r = (float) droneAttitude.getRoll();
-        float p = (float) droneAttitude.getPitch();
-        float y = (float) droneAttitude.getYaw();
-
-        if (!headingModeFPV & y < 0) {
-            y = 360 + y;
-        }
-
-        attitudeIndicator.setAttitude(r, p, y);
-
-        roll.setText(String.format("%3.0f\u00B0", r));
-        pitch.setText(String.format("%3.0f\u00B0", p));
-        yaw.setText(String.format("%3.0f\u00B0", y));
-
-    }
-
-    private void onSpeedUpdate(Speed speed) {
-        if (speed != null) {
-            final SpeedUnitProvider speedUnitProvider = getSpeedUnitProvider();
-
-            airSpeed.setText(speedUnitProvider.boxBaseValueToTarget(speed.getAirSpeed()).toString());
-            groundSpeed.setText(speedUnitProvider.boxBaseValueToTarget(speed.getGroundSpeed()).toString());
-            climbRate.setText(speedUnitProvider.boxBaseValueToTarget(speed.getVerticalSpeed()).toString());
-        }
-    }
-
-    private void onAltitudeUpdate(Altitude altitude) {
-        if (altitude != null) {
-            double alt = altitude.getAltitude();
-            LengthUnit altUnit = getLengthUnitProvider().boxBaseValueToTarget(alt);
-
-            this.altitude.setText(altUnit.toString());
-        }
-    }
+//    public void onOrientationUpdate(Attitude droneAttitude) {
+//        if (droneAttitude == null)
+//            return;
+//
+//        float r = (float) droneAttitude.getRoll();
+//        float p = (float) droneAttitude.getPitch();
+//        float y = (float) droneAttitude.getYaw();
+//
+//        if (!headingModeFPV & y < 0) {
+//            y = 360 + y;
+//        }
+//
+//        attitudeIndicator.setAttitude(r, p, y);
+//
+//        roll.setText(String.format("%3.0f\u00B0", r));
+//        pitch.setText(String.format("%3.0f\u00B0", p));
+//        yaw.setText(String.format("%3.0f\u00B0", y));
+//
+//    }
+//
+//    private void onSpeedUpdate(Speed speed) {
+//        if (speed != null) {
+//            final SpeedUnitProvider speedUnitProvider = getSpeedUnitProvider();
+//
+//            airSpeed.setText(speedUnitProvider.boxBaseValueToTarget(speed.getAirSpeed()).toString());
+//            groundSpeed.setText(speedUnitProvider.boxBaseValueToTarget(speed.getGroundSpeed()).toString());
+//            climbRate.setText(speedUnitProvider.boxBaseValueToTarget(speed.getVerticalSpeed()).toString());
+//        }
+//    }
+//
+//    private void onAltitudeUpdate(Altitude altitude) {
+//        if (altitude != null) {
+//            double alt = altitude.getAltitude();
+//            LengthUnit altUnit = getLengthUnitProvider().boxBaseValueToTarget(alt);
+//
+//            this.altitude.setText(altUnit.toString());
+//        }
+//    }
 }
